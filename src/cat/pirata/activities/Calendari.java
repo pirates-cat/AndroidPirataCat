890cccb6638677017f025a92248239d77e50a536
m13 <sergio.arcos@gmail.com>
2011-01-20 15:12:46 +0100

    nou calendari

--- src/cat/pirata/activities/Calendari.java
+++ src/cat/pirata/activities/Calendari.java
@@ -8,10 +8,12 @@ public class Calendari extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		String url = "https://www.google.com/calendar/hosted/dario.im/" +
-				"embed?showTitle=0&showNav=0&showPrint=0&showCalendars=0&showTz=0&mode=AGENDA" +
-				"&height=300&wkst=2&bgcolor=%23FFFFFF&src=998h7jra4pnmlorrm8vg7hhaho%40group.calendar.google.com" +
-				"&color=%23182C57&ctz=Europe%2FMadrid";
+		String url = "https://www.google.com/calendar/hosted/dario.im/embed" +
+			"?hl=ca&title=Pirates%20de%20Catalunya%20(CAT)" +
+			"&src=998h7jra4pnmlorrm8vg7hhaho@group.calendar.google.com" + // Catalunya
+			"&mode=AGENDA&height=300&wkst=2&bgcolor=%23FFFFFF" +
+			"&showTitle=0&showNav=0&showPrint=0&showCalendars=0&showTz=0" +
+			"&color=%23182C57&ctz=Europe%2FMadrid";
 		WebView webview = new WebView(this);
 		webview.getSettings().setJavaScriptEnabled(true);
 
